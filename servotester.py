483e9aa83a99456f2ec6c0de5eebfc71c016e086
karnschi <karnschi@gmx.at>
2021-03-12 22:47:40 +0100

    debugging

--- servotester.py
+++ servotester.py
@@ -8,15 +8,17 @@ from lib_pwm import PWM_Modul
 angle = 0
 minAngle = float(6.3)
 maxAngle = float(10)
+turning = False
 
 def twistingThread(servo):#, minAngle, maxAngle):
     try:
         global angle
         global minAngle
         global maxAngle
+        global turning
         position = minAngle
         print(str(minAngle) + "  " + str(maxAngle))
-        while(angle < 1000):
+        while(angle < 1000 and turning):
             minA = minAngle
             maxA = maxAngle
 
@@ -56,27 +58,43 @@ def testRun():
     global angle
     global minAngle
     global maxAngle
+    global turning
     turning = False
+    t = False
     manual = True
     position = 8
     try:
-        while(manual):
-            position = float(input("Neuer Wert: "))
-            if position > 500:
-                break
-            servo.setServoPosition(position)
+        while(True):
+            while(manual):
+                position = float(input("Neuer Wert: "))
+                if position == 123:
+                    turning = True
+                    t = True
+                    manual = False
+                if position > 500:
+                    break
+                servo.setServoPosition(position)
 
-        while(turning):
-            minAngle = float(input(str(minAngle) + " und neues Minimum: "))
-            maxAngle = float(input(str(maxAngle) + " und neues Maximum: "))
+            while(turning):
+                if t:
+                    twister1 = threading.Thread(target=twistingThread, args=[servo1])  # , 3, 15])
+                    twister1.start()
+                    t = False
 
-            if minAngle > 100 or maxAngle > 100:
-                break
+                minAngle = float(input(str(minAngle) + " und neues Minimum: "))
+                maxAngle = float(input(str(maxAngle) + " und neues Maximum: "))
 
-            if minAngle < 1.0:
-                minAngle = 1.0
-            if maxAngle > 16.0:
-                maxAngle = 16.0
+                if minAngle == 123 or maxAngle == 123:
+                    manual = True
+                    turning = False
+
+                if minAngle > 500 or maxAngle > 500:
+                    break
+
+                if minAngle < 1.0:
+                    minAngle = 1.0
+                if maxAngle > 16.0:
+                    maxAngle = 16.0
         angle = 1000
         bus.end()
 
