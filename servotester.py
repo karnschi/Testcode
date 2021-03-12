15bc560be39f1c7336da04ec2b8208ea63726ea0
karnschi <karnschi@gmx.at>
2021-03-12 21:56:14 +0100

    debugging ;)

--- servotester.py
+++ servotester.py
@@ -20,7 +20,7 @@ def twistingThread(servo):#, minAngle, maxAngle):
             minA = minAngle
             maxA = maxAngle
 
-            steps = 2
+            steps = 1
             turningtime = 1
             timestep = turningtime / steps
             step = (maxA - minA) / steps
