e2602ac05898cf81f0802a7be5c260cb3abf5f14
karnschi <karnschi@gmx.at>
2021-04-03 21:49:27 +0200

    gyro tester added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import servotester
+
 import lib_car
 import lib_pwm
 import lib_i2c_handle
 from lib_i2c_handle import I2C_Handle
 from lib_gyro import *
+import servotester
 
 if __name__ == '__main__':
     try:
