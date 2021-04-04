919457584e6669e027fa779cc3861d6a276e1fb7
karnschi <karnschi@gmx.at>
2021-04-05 00:52:02 +0200

    trying to figure out TCA9548a

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ if __name__ == '__main__':
     try:
         bus = I2C_Handle()
         print(bus.read_byte(0x70, 0x75))
-        print(bus.read_byte(0x75))
-        bus.send_byte(0x75, 0x01, 0x01)
-        print(bus.read_byte(0x75))
+        print(bus.read_byte(0x75, 0x00))
+        bus.send_byte(0x75, 0x01)
+        print(bus.read_byte(0x75, 0x00))
         for i in range(112,120):
             print(i)
-            print(bus.read_byte(i))
+            print(bus.read_byte(i, 0x00))
         #servotester.testRun(bus)
 
         gyrotester(bus)
