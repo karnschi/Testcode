29fc0fc85a9687c43ec7e25585b680a112c3e800
karnschi <karnschi@gmx.at>
2021-04-04 23:43:32 +0200

    trying to figure out TCA9548a

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ def gyroServo(bus):
 if __name__ == '__main__':
     try:
         bus = I2C_Handle()
+        print(bus.read_byte(0x75))
         bus.send_byte(0x75, 0x01)
+        print(bus.read_byte(0x75))
         #servotester.testRun(bus)
 
         gyrotester(bus)
